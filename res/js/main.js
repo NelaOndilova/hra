f00e2c9d3ade38028461618116a3df60785d3b80
NelaOndilova_skola_pc <nela.ondilova.@student.spsmb.cz>
2025-04-03 13:35:52 +0200

    added functions

--- res/js/main.js
+++ res/js/main.js
@@ -7,7 +7,7 @@ class Player {
     this.c = c;
     this.v = 5;
     this.a = 10;
-    //velocity
+    satisfies
   }
 
   draw(ctx) {
@@ -16,10 +16,27 @@ class Player {
   }
 
   update(keys) {
-    if (keys["w"]) this.y -= this.v;
-    if (keys["s"]) this.y += this.v;
-    if (keys["a"]) this.x -= this.v;
-    if (keys["d"]) this.x += this.v;
+    if (keys["w"]) {
+      if (this.y - this.v > 0){
+        this.y -= this.v;
+      }
+    }
+    if (keys["s"]){
+      if (this.y + this.h + this.v < gameCanvas.height){
+        this.y += this.v;
+      }
+    } 
+    if (keys["a"]) {
+      if (this.x - this.v > 0){
+        this.x -= this.v;
+      }
+    }
+    if (keys["d"]){
+      if (this.x + this.w + this.v < gameCanvas.width){
+        this.x += this.v;
+      }
+    }
+
     if (keys[" "]) {
       this.w -= 2;
       this.h -= 2;
@@ -48,18 +65,32 @@ resizeCanvas();
 
 let keys = {};
 
-//                             e.key = w, do keys se prida w: true
 window.addEventListener("keydown", (e) => (keys[e.key] = true));
 window.addEventListener("keyup", (e) => (keys[e.key] = false));
 
+const testObject = {
+  w: 100,
+  h: 100,
+  x: 200,
+  y: 300,
+  c: "blue"
+}
+
 const gameLoop = () => {
   ctx.clearRect(0, 0, gameCanvas.width, gameCanvas.height);
+  myPlayer.update(keys); 
 
-  //update
-  myPlayer.update(keys);
-  //draw
   myPlayer.draw(ctx);
+  ctx.fillStyle = testObject.c;
+  ctx.fillRect(testObject.x, testObject.y, testObject.w, testObject.h)
+  checkCollision(myPlayer, testObject);
   requestAnimationFrame(gameLoop);
 };
 
 requestAnimationFrame(gameLoop);
+
+const checkCollision = (object1, object2) =>{
+  if(object1.x + object1.w > object2.x && object1.x < object2.x + object2.w && object1.y < object2.y + object2.h && object1.y + object1.h > object2.y){
+    console.log("Collision detected, opinion rejected");
+  } 
+}
